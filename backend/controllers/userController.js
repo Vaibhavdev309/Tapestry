83567fc939f43ee082d296d5e4ed88f14666edce
Vaibhav Kumar Maurya <vaibhav.dev.309@gmail.com>
2025-01-20 02:52:28 +0530

    Added user login register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,62 @@
-const loginUser = async (req, res) => {};
+import userModel from "../models/userModel.js";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-const registerUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "User doesn't exist" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      const token = createToken(user._id);
+      res.json({ sucess: true, token });
+    } else {
+      res.json({ success: false, message: "Password not match" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please enter a valid Email",
+      });
+    }
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Please enter a strong password",
+      });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new userModel({ name, email, password: hashedPassword });
+    const user = await newUser.save();
+    const token = createToken(user._id);
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 const adminLogin = async () => {};
 
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRouter.js";
 
 //App config
 const app = express();
@@ -13,6 +15,9 @@ connectCloudinary();
 // Middlewares
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
 
 //API Endpoints
 app.get("/", (req, res) => {
