48436fe6eecf56cc366a4740a42ba22fc1ac70e0
Vaibhav Kumar Maurya <vaibhav.dev.309@gmail.com>
2025-01-21 12:05:44 +0530

    Completed Admin Frontend

---
+++ admin/src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Add from "./pages/add";
+import List from "./pages/list";
+import Orders from "./pages/orders";
+import Login from "./components/Login";
+const App = () => {
+  const [token, setToken] = useState("");
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      {token === "" ? (
+        <Login />
+      ) : (
+        <>
+          <Navbar />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/orders" element={<Orders />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
